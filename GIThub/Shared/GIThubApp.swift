476c95a43a695aea7cdb67bc760ebdd6b122c0b6
sandeep <pavansandeep65@github.com>
2021-03-30 07:02:43 -0700

    hello this is my first commit.

---
+++ GIThub/Shared/GIThubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIThubApp.swift
+//  Shared
+//
+//  Created by Sandeep Lavanam on 2021-03-29.
+//
+
+import SwiftUI
+
+@main
+struct GIThubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/TailorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TailorApp.swift
+//  Shared
+//
+//  Created by Sandeep Lavanam on 2021-03-15.
+//
+
+import SwiftUI
+
+@main
+struct TailorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
